fdea0138895682606f9afc95d17cac0ca41d6190
Rostyslav Kovalenko <kovalenkorostik14@gmail.com>
2024-12-05 04:48:09 +0200

    initial commit

---
+++ src/FirstClass.java
@@ -0,0 +1,5 @@
+public class FirstClass {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+FirstClass resoult = new FirstClass();
+int resoult1 = resoult.sum(5,6);
+System.out.print(resoult1+ " \nсосал\nбольшой\nпенис");
+    }
+}
